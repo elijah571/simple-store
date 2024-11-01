ca1b2fabe0886d45557f5a5d1808bb5cafe58d5b
elijah571 <elijah571>
2024-11-01 05:07:56 -0700

    product model created and image folder with images added

---
+++ backend/models/products.model.js
@@ -0,0 +1,28 @@
+// models/Product.js
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true
+    }
+});
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
