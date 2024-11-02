cb10c1e59d81b07d334929d8851f61cfa939e5bd
elijah571 <elijah571>
2024-11-02 09:19:45 -0700

    header

--- frontend/index.js
+++ frontend/index.js
@@ -3,9 +3,7 @@ async function getAllProducts() {
     try {
          const response = await fetch("http://localhost:3000/api/store/products"); 
         const products = await response.json();
-        console.log(products);
-
-        let 
+        console.log(products); 
             
     } catch (error) {
 
