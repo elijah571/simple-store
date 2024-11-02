bb5742a849f8f33bbaba3a4df668bd9eba25763d
elijah571 <elijah571>
2024-11-02 08:32:37 -0700

    created the header section and hero

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 // Importing Node.js dependencies
 import express from "express";
+import cors from 'cors';
 import dotenv from "dotenv";
 // Importing custom files
 import { connectDB } from "./dataBase/connectDb.js";
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middlewars
 app.use(express.json());
+app.use(cors());
 app.use(express.static('images')); // Serve images folder statically
 
 // Connect to the databases
